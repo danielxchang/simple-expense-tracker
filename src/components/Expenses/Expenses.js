505f14b0ea5319b46b9129d428f68e0a7a56c049
Daniel Chang <daniel.changk@gmail.com>
2022-02-21 17:17:26 -0600

    adding additional components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,18 +1,36 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState("2022");
   const { items } = props;
+
   const expenseItems = items.map((exp, i) => (
     <ExpenseItem
-      key={"expenses" + i}
+      key={exp.id}
       title={exp.title}
       amount={exp.amount}
       date={exp.date}
     />
   ));
-  return <Card className="expenses">{expenseItems}</Card>;
+
+  const filterExpensesHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        year={filterYear}
+        onFilterExpenses={filterExpensesHandler}
+      />
+      {expenseItems}
+    </Card>
+  );
 };
 
 export default Expenses;
